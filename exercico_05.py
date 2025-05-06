37232c3955ae67f9cabb32b8f75f8525c8c76fda
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-05-06 12:07:26 -0300

    exercico 5

--- exercico_05.py
+++ exercico_05.py
@@ -1,11 +1,19 @@
-def cadastrar(nome, email, Serie):
+def cadastrar(nome, email, Serie, nota01, nota02, nota03):
     lista = []
     dicionario = {
         "nome" : nome, 
         "email" : email,
-        "serie" : Serie 
+        "serie" : Serie, 
+        "notas" : [nota01, nota02, nota03]
         }
     lista.append(dicionario)
     return dicionario
 
-print(cadastrar("Nicholas", "nicholasmoreti65@gmail.com", "2TB"))
+print (cadastrar(
+        "Nicholas", 
+        "nicholasmoreti65@gmail.com", 
+        "2TB",
+        10,
+        6,
+        9
+    ))
