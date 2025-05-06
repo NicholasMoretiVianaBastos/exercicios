a9f642658539f9587af2c575407645a877b960a8
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-05-06 08:30:49 -0300

    exercico 4

---
+++ exercico_04.py
@@ -0,0 +1,11 @@
+def somar_numeros(lista):
+    soma = 0
+    for i in lista:
+        soma += i
+    return soma
+
+numeros_aleatorios = [15, 26, 33, 49, 56]
+resultado = somar_numeros(numeros_aleatorios)
+media = resultado / 5
+
+print(f"Sua media é {media}")
