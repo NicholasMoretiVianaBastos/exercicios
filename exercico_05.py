46713c2feebb260bc6b1804d10f7a7f81307f588
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-05-12 14:18:50 -0300

    exercicio 05

--- exercico_05.py
+++ exercico_05.py
@@ -1,13 +1,16 @@
 from exercico_04 import somar_numeros
 
-def cadastrar(nome, email, Serie, nota01, nota02, nota03):
+def cadastrar(nome, email, Serie, nota01=0, nota02=0, nota03=0):
     lista = []
+    
+    notas = [nota01, nota02, nota03]
+
     dicionario = {
         "nome" : nome, 
         "email" : email,
         "serie" : Serie, 
-        "notas" : [nota01, nota02, nota03], 
-        "media" : somar_numeros
+        "notas" : notas, 
+        "media" : somar_numeros(notas)
         }
     lista.append(dicionario)
     media = somar_numeros(dicionario["nota"])
@@ -17,7 +20,5 @@ print (cadastrar(
         "Nicholas", 
         "nicholasmoreti65@gmail.com", 
         "2TB",
-        10,
-        6,
-        9
+        
     ))
