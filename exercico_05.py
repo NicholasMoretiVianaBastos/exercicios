1103cb156d6ec93864a16dd53845744f18daa4e3
M0reti <00001100109675sp@al.educacao.sp.gov.br>
2025-05-19 14:25:36 -0300

    exercicio 07

--- exercico_05.py
+++ exercico_05.py
@@ -1,24 +1,38 @@
 from exercico_04 import somar_numeros
+   
+lista = []
+
+def obter_dados_aluno():
+    nome_aluno = input("Informe o nome completo do aluno: ")
+    email_aluno = input("Informe o email do aluno: ")
+    serie_aluno = input("Informe a serie do aluno: ")
+    nota01_aluno = int(input("Informe a primeira nota do aluno: "))
+    nota02_aluno = int(input("Informe a segunda nota do aluno: "))
+    nota03_aluno = int(input("Informe a terceira nota do aluno: "))
+   
+    return cadastrar(nome_aluno, email_aluno, serie_aluno, nota01_aluno, nota02_aluno, nota03_aluno)
 
 def cadastrar(nome, email, Serie, nota01=0, nota02=0, nota03=0):
-    lista = []
     
     notas = [nota01, nota02, nota03]
 
     dicionario = {
         "nome" : nome, 
-        "email" : email,
+        "email" : email,      
         "serie" : Serie, 
         "notas" : notas, 
         "media" : somar_numeros(notas)
         }
     lista.append(dicionario)
-    media = somar_numeros(dicionario["nota"])
+    media = somar_numeros(dicionario["notas"])
     return dicionario
 
-print (cadastrar(
-        "Nicholas", 
-        "nicholasmoreti65@gmail.com", 
-        "2TB",
-        
-    ))
+obter_dados_aluno()
+
+def mostrar_dados_alunos(dados_alunos):
+    for lista in dados_alunos:
+        print(f"Nome do aluno: {lista["nome"]}")
+
+    return 
+
+mostrar_dados_alunos(lista)
